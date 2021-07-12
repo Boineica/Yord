18338112480621646775e45243c9ff39afd96e7e
Vorser-alt <85923289+Vorser-alt@users.noreply.github.com>
2021-07-12 11:24:21 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+from keep_alive import keep_alive
+import discord
+import random
+import asyncio
+from discord.ext import commands
+
+
+
+bot = commands.Bot(command_prefix='y!')
+
+@bot.command()
+async def about(ctx):
+    print("The command \"y!about\" was used ")
+    await ctx.send('Hi!\nI\'m Yord!\nI\'m some bot that uhh...\n ||Want to join the support server? Soon! || \nI\'m a bot made by Vorser#3186!\n||More to come soon!||')
+
+@bot.command()
+async def chitchot(ctx):
+    print("The command \"y!chitchot\" was used ")
+    await ctx.send("Whoa! You found me! I chose the name \"chitchot\" because i ran out of ideas for names. This is a upcoming command. See ya!\nBy Vorser#3186.")
+    
+@bot.command()
+async def yord2ten(ctx):
+    print("The command \"y!yord2ten\" was used ")
+    await ctx.send("Not again!")
+    
+@bot.command()
+async def randomnumber(ctx):
+    print("The command \"y!randomnumber\" was used ")
+    rndnu = random.randint(1, 9999999)
+
+    print("A random number is generating")
+    await ctx.send("Random number is generating.")
+    
+    print("The random number was generated")
+    await ctx.send("Your generated number is: {}!".format(rndnu))
+
+keep_alive()
+bot.run("ODU0MjMwMTcyOTE1NTk3MzYz.YMg52A.CsES93KAQquuaEFXp53F8BYlB1Y")
